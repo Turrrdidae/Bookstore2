94c8ddbb1375e934f2acb3db4557baf54f3d2562
Turrrdidae <1945257314@qq.com>
2023-12-18 16:40:05 +0800

    修改使用book.db数据库的问题

--- fe/access/book.py
+++ fe/access/book.py
@@ -32,12 +32,14 @@ class Book:
 class BookDB:
     def __init__(self, large: bool = False):
         parent_path = os.path.dirname(os.path.dirname(__file__))
+        # self.db_s = os.path.join(parent_path, "data/book.db")
+        # self.db_l = os.path.join(parent_path, "data/book_lx.db")
+        # if large:
+        #     self.book_db = self.db_l
+        # else:
+        #     self.book_db = self.db_s
         self.db_s = os.path.join(parent_path, "data/book.db")
-        self.db_l = os.path.join(parent_path, "data/book_lx.db")
-        if large:
-            self.book_db = self.db_l
-        else:
-            self.book_db = self.db_s
+        self.book_db = self.db_s
 
     def get_book_count(self):
         conn = sqlite.connect(self.book_db)
