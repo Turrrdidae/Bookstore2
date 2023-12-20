9147641f8de47941f912de6710c18314c874a0b4
Turrrdidae <1945257314@qq.com>
2023-12-20 18:59:49 +0800

    修改be/model/seller.py为mySQL

--- be/model/seller.py
+++ be/model/seller.py
@@ -1,4 +1,5 @@
 import sqlite3 as sqlite
+import mysql.connector
 from be.model import error
 from be.model import db_conn
 
@@ -23,16 +24,25 @@ class Seller(db_conn.DBConn):
             if self.book_id_exist(store_id, book_id):
                 return error.error_exist_book_id(book_id)
 
+            # self.conn.execute(
+            #     "INSERT into store(store_id, book_id, book_info, stock_level)"
+            #     "VALUES (?, ?, ?, ?)",
+            #     (store_id, book_id, book_json_str, stock_level),
+            # )
             self.conn.execute(
                 "INSERT into store(store_id, book_id, book_info, stock_level)"
-                "VALUES (?, ?, ?, ?)",
+                "VALUES (%s, %s, %s, %s)",
                 (store_id, book_id, book_json_str, stock_level),
             )
             self.conn.commit()
-        except sqlite.Error as e:
+        # except sqlite.Error as e:
+        #     return 528, "{}".format(str(e))
+        except mysql.connector.Error as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
+        finally:
+            self.conn.close()
         return 200, "ok"
 
     def add_stock_level(
@@ -46,16 +56,25 @@ class Seller(db_conn.DBConn):
             if not self.book_id_exist(store_id, book_id):
                 return error.error_non_exist_book_id(book_id)
 
+            # self.conn.execute(
+            #     "UPDATE store SET stock_level = stock_level + ? "
+            #     "WHERE store_id = ? AND book_id = ?",
+            #     (add_stock_level, store_id, book_id),
+            # )
             self.conn.execute(
-                "UPDATE store SET stock_level = stock_level + ? "
-                "WHERE store_id = ? AND book_id = ?",
+                "UPDATE store SET stock_level = stock_level + %s "
+                "WHERE store_id = %s AND book_id = %s",
                 (add_stock_level, store_id, book_id),
             )
             self.conn.commit()
-        except sqlite.Error as e:
+        # except sqlite.Error as e:
+        #     return 528, "{}".format(str(e))
+        except mysql.connector.Error as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
+        finally:
+            self.conn.close()
         return 200, "ok"
 
     def create_store(self, user_id: str, store_id: str) -> (int, str):
@@ -64,13 +83,21 @@ class Seller(db_conn.DBConn):
                 return error.error_non_exist_user_id(user_id)
             if self.store_id_exist(store_id):
                 return error.error_exist_store_id(store_id)
+            # self.conn.execute(
+            #     "INSERT into user_store(store_id, user_id)" "VALUES (?, ?)",
+            #     (store_id, user_id),
+            # )
             self.conn.execute(
-                "INSERT into user_store(store_id, user_id)" "VALUES (?, ?)",
+                "INSERT into user_store(store_id, user_id)" "VALUES (%s, %s)",
                 (store_id, user_id),
             )
             self.conn.commit()
-        except sqlite.Error as e:
+        # except sqlite.Error as e:
+        #     return 528, "{}".format(str(e))
+        except mysql.connector.Error as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
+        finally:
+            self.conn.close()
         return 200, "ok"
